0698532ff952d6fedcc9b8668d17f1492e3497be
anishazahan <anishazahan13@gmail.com>
2023-08-25 06:24:34 +0600

    crud api

--- src/app/modules/projects/projects.routes.ts
+++ src/app/modules/projects/projects.routes.ts
@@ -1,15 +1,19 @@
-
 import express from "express";
-import { createProject, deleteProjectByID, getProjectByID, getProjects, updateProjectByID } from "./projects.controller";
+import {
+  createProject,
+  deleteProjectByID,
+  getProjectByID,
+  getProjects,
+  updateProjectByID,
+} from "./projects.controller";
 
 const router = express.Router();
 
+router.post("/create-project", createProject);
 
-router.post('/create-project',createProject)
-
-router.get('/',getProjects)
-router.get('/:id',getProjectByID)
-router.patch('/update/:id',updateProjectByID)
-router.delete('/delete/:id',deleteProjectByID)
+router.get("/", getProjects);
+router.get("/:id", getProjectByID);
+router.patch("/update/:id", updateProjectByID);
+router.delete("/delete/:id", deleteProjectByID);
 
 export default router;
