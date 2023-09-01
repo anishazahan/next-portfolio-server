91fca555d91de30bbdc9999aa10da9d87b54da84
anishazahan <anishazahan13@gmail.com>
2023-09-01 12:38:41 +0600

    update

--- dist/app.js
+++ dist/app.js
@@ -8,8 +8,20 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 // application route
 const projects_routes_1 = __importDefault(require("./app/modules/projects/projects.routes"));
-//  using cors
-app.use((0, cors_1.default)());
+// Configure CORS to allow requests from 'http://localhost:3000'
+const allowedOrigins = ["https://next-portfolio-server.vercel.app/"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
+// Use CORS middleware with the specified options
+app.use((0, cors_1.default)(corsOptions));
 // parse data
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,20 @@ const app: Application = express();
 
 import projectRoutes from "./app/modules/projects/projects.routes";
 
-//  using cors
-app.use(cors());
+// Configure CORS to allow requests from 'http://localhost:3000'
+const allowedOrigins = ["https://next-portfolio-server.vercel.app/"];
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// Use CORS middleware with the specified options
+app.use(cors(corsOptions));
 
 // parse data
 app.use(express.json());
